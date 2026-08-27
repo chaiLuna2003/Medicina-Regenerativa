937e176eeb2fd586c14ce9848d32f28e9c0508d6
ChaiLuna2003 <angelunagzl@outlook.com>
2026-08-26 22:50:44 -0600

    Extraer estados de validacion de ficha paciente

--- resources/views/pacientes/show.blade.php
+++ resources/views/pacientes/show.blade.php
@@ -155,74 +155,9 @@
 
 
 
-
-    <div
-        id="estado-validacion-historia"
-        data-tiene-errores="{{ $errors->hasAny([
-        'patologia_base',
-        'padecimiento_actual',
-        'tratamientos_actuales',
-        'prioridad_analisis_medico',
-    ]) ? 'true' : 'false' }}"
-        hidden>
-    </div>
-
-    <div
-        id="estado-validacion-heredofamiliares"
-        data-tiene-errores="{{ (
-        $errors->heredofamiliares->has('numero_hermanos')
-        || $errors->heredofamiliares->has('antecedentes.*')
-    ) ? 'true' : 'false' }}"
-        hidden>
-    </div>
-
-    <div
-        id="estado-validacion-habitos-alimenticios"
-        data-tiene-errores="{{ (
-        $errors
-            ->habitosAlimenticios
-            ->has('comidas.*')
-        || $errors
-            ->habitosAlimenticios
-            ->has('alimentos.*')
-    ) ? 'true' : 'false' }}"
-        hidden>
-    </div>
-
-    <div
-        id="estado-validacion-personales-patologicos"
-        data-tiene-errores="{{ (
-        $errors
-            ->personalesPatologicos
-            ->has('antecedentes.*')
-    ) ? 'true' : 'false' }}"
-        hidden>
-    </div>
-
-    <div
-        id="estado-validacion-personales-no-patologicos"
-        data-tiene-errores="{{ (
-        $errors
-            ->personalesNoPatologicos
-            ->has('antecedentes.*')
-    ) ? 'true' : 'false' }}"
-        hidden>
-    </div>
-
-    <div
-        id="estado-validacion-exploracion-fisica"
-        data-tiene-errores="{{ (
-        $errors->exploracionFisica->hasAny([
-            'interrogatorio',
-            'anotaciones',
-            'recomendaciones',
-        ])
-        || $errors
-            ->exploracionFisica
-            ->has('sistemas.*')
-    ) ? 'true' : 'false' }}"
-        hidden>
-    </div>
+@include(
+    'pacientes.partials.estados-validacion'
+)
 
     <script>
         /*
